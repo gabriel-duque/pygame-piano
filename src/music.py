f9f55341d324c03142e56c37eedd7fbce3b9f6c7
Gabriel Duque <gabriel.duque@lse.epita.fr>
2020-01-21 16:59:26 +0100

    src: add keyboard drawing and updating

--- src/music.py
+++ src/music.py
@@ -10,6 +10,13 @@ with contextlib.redirect_stdout(None):
     import pygame.midi
 
 
+# Colors for keys and background
+color_grey  = (127, 127, 127)
+color_white = (255, 255, 255)
+color_black = (0, 0, 0)
+color_red   = (255, 0, 0)
+color_blue   = (0, 0, 255)
+
 # This is the key mapping for our 'piano' is you use another keymap than qwerty
 # you might want to change some keys.
 # I have annotated the changes to make for the azerty keymap
@@ -22,18 +29,18 @@ key_to_note = {
         # on: a boolean to keep track of if this note is on
 
         # 12 is the pitch for C0 (this isn't on a piano keyboard as they start at A0 (pitch: 21))
-        pygame.K_a: (12, False, False), # C0       (azerty: pygame.K_q)
-        pygame.K_w: (13, True,  False), # C#/Db0   (azerty: pygame.K_z)
-        pygame.K_s: (14, False, False), # D0
-        pygame.K_e: (15, False, False), # D#/Eb0
-        pygame.K_d: (16, True,  False), # E0
-        pygame.K_f: (17, False, False), # F0
-        pygame.K_t: (18, True,  False), # F#/Gb0
-        pygame.K_g: (19, False, False), # G0
-        pygame.K_y: (20, False, False), # G#/Ab0
-        pygame.K_h: (21, True,  False), # A0
-        pygame.K_u: (22, False, False), # A#/Bb0
-        pygame.K_j: (23, True,  False), # B0
+        pygame.K_a: (12, False, False),  # C0       (azerty: pygame.K_q)
+        pygame.K_w: (13, True,  False),  # C#/Db0   (azerty: pygame.K_z)
+        pygame.K_s: (14, False, False),  # D0
+        pygame.K_e: (15, True, False),   # D#/Eb0
+        pygame.K_d: (16, False,  False), # E0
+        pygame.K_f: (17, False, False),  # F0
+        pygame.K_t: (18, True,  False),  # F#/Gb0
+        pygame.K_g: (19, False, False),  # G0
+        pygame.K_y: (20, True, False),   # G#/Ab0
+        pygame.K_h: (21, False,  False), # A0
+        pygame.K_u: (22, True, False),   # A#/Bb0
+        pygame.K_j: (23, False,  False), # B0
 }
 
 # The number of different notes in an octave
@@ -67,6 +74,41 @@ def midi_device_prompt() -> int:
 
     return int(input("Which MIDI device would you like to use ? "))
 
+def draw_keyboard(window) -> None:
+    '''Draw the octave keyboard on screen'''
+
+    margin = window.get_width() // 140
+
+    white_width = (window.get_width() - 8 * margin) // 7
+    black_width = white_width // 2
+
+    left = margin
+    top = margin
+    bottom = window.get_height() - top
+
+    # Draw white keys
+    for key in key_to_note:
+        note, black, on = key_to_note[key]
+        if black:
+            continue
+        pygame.draw.rect(window, color_red if on else color_white, (left, top, white_width, bottom - top))
+        left += margin + white_width
+
+    # Reset left for black keys
+    left = margin + white_width + margin // 2 - black_width // 2
+
+    bottom = bottom - (bottom - top) // 3
+
+    # Draw black keys
+    for key in key_to_note:
+        note, black, on = key_to_note[key]
+        if not black:
+            continue
+        if note == 18:  # Skip the inexistant black key between E and F
+            left += margin + white_width
+        pygame.draw.rect(window, color_blue if on else color_black, (left, top, black_width, bottom - top))
+        left += margin + white_width
+
 def main() -> None:
     '''The main entrypoint for our program'''
 
@@ -82,17 +124,18 @@ def main() -> None:
         print(f'Usage: {os.path.basename(sys.argv[0])} [MIDI_DEVICE]')
         sys.exit(os.EX_USAGE)
 
-    # Initialize the display
     pygame.display.init()
     pygame.display.set_caption('Girls can code (and play music)!')
-    # XXX: actually set size of window and display initial keyboard
-    pygame.display.set_mode((190, 1))
+    window = pygame.display.set_mode((1280, 720))
+    window.fill(color_grey)
+    draw_keyboard(window)
+    pygame.display.update()
 
     # We only want to get events for our keyboard
     # Let's disable the others for performance
     # XXX: fix allowed events
     pygame.event.set_allowed(None)
-    pygame.event.set_allowed(pygame.KEYDOWN)
+    pygame.event.set_allowed([pygame.KEYDOWN, pygame.KEYUP])
 
     # Create our MIDI handle
     print(f'Setting MIDI device to {midi_dev}')
@@ -131,7 +174,8 @@ def main() -> None:
                         midi_hndl.note_off(note + note_count * octave, velocity)
                         key_to_note[key] = note, black, False
 
-        # XXX: Update our screen to show which keys are pressed
+        draw_keyboard(window)
+        pygame.display.update()
 
 if __name__ == '__main__':
     main()
